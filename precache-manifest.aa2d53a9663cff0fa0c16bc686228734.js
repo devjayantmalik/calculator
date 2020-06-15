87705ba69b5bfeb4d8a5d02aba1e8dd9407e3676
Jayant Malik <dev.jayantmalik@gmail.com>
2020-06-15 20:03:57 +0530

    Updates

---
+++ precache-manifest.aa2d53a9663cff0fa0c16bc686228734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6811995ce69ba51397b1f16bf1a928af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01aed302bdb265518718",
+    "url": "./static/js/2.f6193861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f6193861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c520c0a5d5ef29ca33a9",
+    "url": "./static/js/main.368c9120.chunk.js"
+  },
+  {
+    "revision": "5b155b1e54ab7e2dd0f2",
+    "url": "./static/js/runtime-main.dbea8c01.js"
+  }
+]);
